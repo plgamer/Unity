51c8effda74aaa9a9627a85365c1e32313c6e351
plgamer <50041233+plgamer@users.noreply.github.com>
2019-07-26 18:16:31 +0800

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    ///<summary>
+    ///3秒后回收到对象池
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator AutoRecyle()
+    {
+        yield return new WaitForSeconds(1f);
+        ObjectPool.GetInstance().Recycleobj(gameObject);
+    }
+    private void OnEnable()
+    {
+        StartCoroutine(AutoRecyle());
+    }
+}
