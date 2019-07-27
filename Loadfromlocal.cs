69e39e0e6639cfee0d2e1109f39ccd55d776e9f9
plgamer <50041233+plgamer@users.noreply.github.com>
2019-07-28 07:42:37 +0800

    assetbundle/loadfromlocal
    从本地assetbundle加载资源的方法实现

---
+++ Loadfromlocal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class Loadfromlocal : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(Loadplayership());
+    }
+    private IEnumerator Loadplayership()
+    {
+
+
+        AssetBundle ps = AssetBundle.LoadFromFile("D:/unityfiles/AssetBundles/model_ab/playership.ab");
+        GameObject obj = ps.LoadAsset<GameObject>("vehicle_playerShip.FBX");
+        Instantiate(obj);
+        yield return 0;
+    }
+
+}
