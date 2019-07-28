24698b09ae9e0d582040392f6dbd1d1bbb6ab9c5
plgamer <50041233+plgamer@users.noreply.github.com>
2019-07-28 22:34:31 +0800

    Create UIbuttonLarger.cs

---
+++ UIbuttonLarger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIbuttonLarger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    
+    void Start()
+    {
+        gameObject.GetComponent<Animator>().enabled=false;
+    }
+    public void OnPointerEnter(PointerEventData eventData)//鼠标进入按钮触发音效和动画
+    {
+
+        gameObject.GetComponent<Animator>().enabled = true;
+     
+            
+    
+    }
+    //鼠标离开时关闭动画
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Animator>().enabled = false;
+        gameObject.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+       }
+
+    
+}
+
